837fe87d01283978d7c76c843ea11dcaaf83b5c6
aatje92 <adriaan.conijn@gmail.com>
2014-07-13 11:53:53 +0200

    Update port.sh

--- port.sh
+++ port.sh
@@ -1,3 +1,7 @@
+if [ ! -f ~/.pia_client_id ]; then
+head -n 100 /dev/urandom | md5sum | tr -d " -" > ~/.pia_client_id
+else
+
 ip=$(ifconfig tun0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 curl -d "user=YOUR_PIA_USERNAME&pass=YOUR_PIA_PASSWORD&client_id=$(cat ~/.pia_client_id)&local_ip=$ip" https://www.privateinternetaccess.com/vpninfo/port_forward_assignment
 
