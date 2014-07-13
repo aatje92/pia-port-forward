5d4a96175b6f312fd2383d71e8755d8ede98d7ea
aatje92 <adriaan.conijn@gmail.com>
2014-07-13 11:55:13 +0200

    Update port.sh

--- port.sh
+++ port.sh
@@ -5,3 +5,4 @@ else
 ip=$(ifconfig tun0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 curl -d "user=YOUR_PIA_USERNAME&pass=YOUR_PIA_PASSWORD&client_id=$(cat ~/.pia_client_id)&local_ip=$ip" https://www.privateinternetaccess.com/vpninfo/port_forward_assignment
 
+fi
