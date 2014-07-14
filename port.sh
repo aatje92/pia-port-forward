ccbbbed402340bd10db1243be867dc88d9b87740
aatje92 <adriaan.conijn@gmail.com>
2014-07-14 08:08:20 +0200

    removed spaces at end of file

--- port.sh
+++ port.sh
@@ -14,7 +14,3 @@ ip=$(ifconfig tun0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 curl -d "user=$uname&pass=$pass&client_id=$(cat ~/.pia_client_id)&local_ip=$ip" https://www.privateinternetaccess.com/vpninfo/port_forward_assignment
 
 fi
-
-
-
-
